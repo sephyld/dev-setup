1d9056c0f6d30cb6a8b3d42fdee8967530e87ea0
Giuseppe B. <85199411+sephyld@users.noreply.github.com>
2024-11-17 16:18:30 +0100

    Support for Linux Mint

--- software/docker.sh
+++ software/docker.sh
@@ -24,7 +24,7 @@ sudo chmod a+r /etc/apt/keyrings/docker.asc
 # Add the repository to Apt sources:
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  $(. /etc/os-release && V_NAME=$VERSION_CODENAME && OS_ID=`lsb_release -is` && if [ $OS_ID != "Ubuntu" ]; then V_NAME=$UBUNTU_CODENAME; fi && echo "$V_NAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 
