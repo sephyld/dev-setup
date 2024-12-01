7100ed563c7831fe72cf038126b23de7dcb45f66
sephyld <sephyld@gmail.com>
2024-12-01 15:45:56 +0100

    lazygit and lazyvim

---
+++ standard/lazyvim.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+sudo rm -rf /opt/nvim
+sudo tar -C /opt -xzf nvim-linux64.tar.gz
+rm nvim-linux64.tar.gz
+
+echo '#nvim' >>~/.bashrc
+echo 'export PATH="$PATH:/opt/nvim-linux64/bin' >>~/.bashrc
+
+# required
+mv ~/.config/nvim{,.bak}
+
+# optional but recommended
+mv ~/.local/share/nvim{,.bak}
+mv ~/.local/state/nvim{,.bak}
+mv ~/.cache/nvim{,.bak}
+
+git clone https://github.com/LazyVim/starter ~/.config/nvim
+rm -rf ~/.config/nvim/.git
+
