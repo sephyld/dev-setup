7a99e57345fc69dd99f3667e92f97ec1ead06b35
sephyld <sephyld@gmail.com>
2024-12-08 13:04:23 +0100

    checking presence before backup and quotes fix

--- standard/lazyvim.sh
+++ standard/lazyvim.sh
@@ -6,17 +6,27 @@ sudo tar -C /opt -xzf nvim-linux64.tar.gz
 rm nvim-linux64.tar.gz
 
 echo '#nvim' >>~/.bashrc
-echo 'export PATH="$PATH:/opt/nvim-linux64/bin' >>~/.bashrc
+echo 'export PATH="$PATH:/opt/nvim-linux64/bin"' >>~/.bashrc
 
 TIME_STAMP=$(date '+%Y%m%d%H%M%S')
 
 # required
-mv ~/.config/nvim{,.bak.$TIME_STAMP}
+if ls ~/.config/nvim >/dev/null; then
+  mv ~/config/nvim{,.bak.$TIME_STAMP}
+fi
 
 # optional but recommended
-mv ~/.local/share/nvim{,.bak.$TIME_STAMP}
-mv ~/.local/state/nvim{,.bak.$TIME_STAMP}
-mv ~/.cache/nvim{,.bak.$TIME_STAMP}
+if ls ~/.local/share/nvim >/dev/null; then
+  mv ~/.local/share/nvim{,.bak.$TIME_STAMP}
+fi
+
+if ls ~/.local/state/nvim >/dev/null; then
+  mv ~/.local/state/nvim{,.bak.$TIME_STAMP}
+fi
+
+if ls ~/.cache/nvim >/dev/null; then
+  mv ~/.cache/nvim{,.bak.$TIME_STAMP}
+fi
 
 git clone https://github.com/LazyVim/starter ~/.config/nvim
 rm -rf ~/.config/nvim/.git
