e4e3611b7e6cfe19c8b7136129a772c964ceba04
sephyld <sephyld@gmail.com>
2024-12-22 20:59:56 +0100

    node installation script formatting

--- languages/node.sh
+++ languages/node.sh
@@ -2,19 +2,19 @@
 # https://mise.jdx.dev/getting-started.html#_3-adding-tools-to-mise
 
 if [[ $SELECTED_GUM_LANGUAGES != *"Node.js"* ]]; then
-    return;
+  return
 fi
 
 # check if already installed
-if ~/.local/bin/mise ls | grep node && ! ~/.local/bin/mise ls node | grep missing > /dev/null; then
-    echo "Node already installed"
-    return;
+if ~/.local/bin/mise ls | grep node && ! ~/.local/bin/mise ls node | grep missing >/dev/null; then
+  echo "Node already installed"
+  return
 fi
 
 echo "Node not installed. Installing Node..."
 
 ~/.local/bin/mise use --global node@22.9.0
 
-echo "`node -v`"
+echo "$(node -v)"
 
 echo "Nodejs installed successfully."
