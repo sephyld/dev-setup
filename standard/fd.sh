934697a888f98e6dd1e2c77a64eed09ab6a1ee1a
sephyld <sephyld@gmail.com>
2024-12-26 17:03:36 +0100

    using single script for some apt installs

--- standard/fd.sh
+++ standard/fd.sh
@@ -1,21 +1,7 @@
-#! /bin/bash
-
-# check if already installed
-if which fdfind >/dev/null; then
-  echo "fdfind already installed. Showing version"
-  echo "$(fdfind --version)"
-  return
-fi
-
-echo "fdfind not installed. Installing Git..."
-
-sudo apt-get update
-sudo apt-get install -y fd-find
-
-echo "$(fdfind --version)"
+#!/usr/bin/env bash
 
 if which fd >/dev/null; then
   ln -s $(which fdfind) ~/.local/bin/fd
 fi
 
-echo "fdfind installed successfully."
+echo "fdfind linked successfully."
