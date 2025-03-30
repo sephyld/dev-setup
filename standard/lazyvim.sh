f6b1c0b66f7ddd7b80105087d7ed6ad17d202e77
sephyld <sephyld@gmail.com>
2025-03-31 01:25:58 +0200

    nvim installation update

--- standard/lazyvim.sh
+++ standard/lazyvim.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
 sudo rm -rf /opt/nvim
-sudo tar -C /opt -xzf nvim-linux64.tar.gz
-rm nvim-linux64.tar.gz
+sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+rm nvim-linux-x86_64.tar.gz
 
 echo '#nvim' >>~/.bashrc
-echo 'export PATH="$PATH:/opt/nvim-linux64/bin"' >>~/.bashrc
+echo 'export PATH="$PATH:/opt/nvim-linux-x86_64/bin"' >>~/.bashrc
 
 TIME_STAMP=$(date '+%Y%m%d%H%M%S')
 
