00fbc4b73ee29ed94e533b430ad06f8f176cdd5e
sephyld <sephyld@gmail.com>
2025-05-16 00:58:09 +0200

    allow selection of branch from boot script

--- boot.sh
+++ boot.sh
@@ -1,4 +1,6 @@
 set -e
+export DEFAULT_BRANCH="main"
+export BRANCH="${BRANCH:-$DEFAULT_BRANCH}"
 
 echo "Begin installation (or abort with ctrl+c)..."
 
@@ -9,5 +11,6 @@ rm -rf ~/.local/share/dev-setup
 git clone https://github.com/sephyld/dev-setup.git ~/.local/share/dev-setup >/dev/null
 
 echo "Starting setup..."
-(cd ~/.local/share/dev-setup && bash setup.sh && cd -)
+echo "Selected branch: $BRANCH"
+(cd ~/.local/share/dev-setup && git checkout $BRANCH && bash setup.sh && cd -)
 rm -rf ~/.local/share/dev-setup
